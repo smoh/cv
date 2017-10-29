8093e29d4d9b0ae2935665d66ddfa02928b83012
Semyeong Oh <semyeong.oh@gmail.com>
2017-10-29 18:56:30 -0400

    update build forumla

--- build-paper.sh
+++ build-paper.sh
@@ -16,12 +16,14 @@ then
 
   # Build the paper using tectonic
   tectonic cv.tex --print
+  tectonic pubs.tex --print
+  pdfunite cv.pdf pubs.pdf cv_pubs.pdf
 
-  # Force push the paper to GitHub
-  cd $TRAVIS_BUILD_DIR
-  git checkout --orphan $TRAVIS_BRANCH-pdf
-  git rm -rf .
-  git add -f cv.pdf
-  git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
-  git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
-fi
+  # # Force push the paper to GitHub
+  # cd $TRAVIS_BUILD_DIR
+  # git checkout --orphan $TRAVIS_BRANCH-pdf
+  # git rm -rf .
+  # git add -f cv.pdf
+  # git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
+  # git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
+# fi

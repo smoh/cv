4419738b0f89ccce0e150dc9680deb629f7271a7
Semyeong Oh <semyeong.oh@gmail.com>
2017-10-29 19:17:39 -0400

    try again; old setup

--- build-paper.sh
+++ build-paper.sh
@@ -16,7 +16,7 @@ then
 
   # Build the paper using tectonic
   tectonic cv.tex --print
-  tectonic pubs.tex --print
+  #tectonic pubs.tex --print
   #pdfunite cv.pdf pubs.pdf cv_pubs.pdf
 
   # # Force push the paper to GitHub
